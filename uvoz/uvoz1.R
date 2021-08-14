358059e49f62413c4bfb7d51453e640da42def36
Marija Janeva <mary.janeva@gmail.com>
2021-08-14 21:29:02 +0200

    Uvoz podatkov

--- uvoz/uvoz1.R
+++ uvoz/uvoz1.R
@@ -10,14 +10,21 @@ library(tidyverse)
 #Tabela 1: Notranje selitve po regijah
 
 selitve_po_regijah <- read.csv("podatki/selitve_po regijah.csv", 
-                               skip = 2, sep =",",
-                               locale = locale(encoding="Windows-1250"))
+                               skip = 2, sep =",")
 
 
 
 #Tabela 2: Notranje selitve po starostnih skupinah
 
-selitve_po_starostnih_skupinah <- read.csv2("podatki/selitve_po starostnih skupinah.csv",
-                                            header = TRUE, skip = 2, sep = ",",
-                                            locale=locale(encoding="Windows-1250"))
+selitve_po_starostnih_skupinah <- read.csv("podatki/selitve_po starostnih skupinah.csv",
+                                            header = TRUE, skip = 2, sep = ",")
 
+#Tabela 3: Stanovanjske razmere
+
+stanovanjske_razmere <- read.csv("podatki/stanovanjske razmere.csv",
+                                 header = TRUE, skip = 2, sep =",")
+
+#Tabela 4: Stanovanjski stroški
+
+stanovanjski_stroski <- read.csv("podatki/stanovanjski stroski.csv", 
+                                 header = TRUE, skip = 2, sep =",")
