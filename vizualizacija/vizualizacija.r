2ea38d7b45f17885b8db47ac03eec1b0696ae4de
Marija Janeva <mary.janeva@gmail.com>
2021-08-19 13:41:55 +0200

    risanje grafov

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -1,8 +1,6 @@
 # 3. faza: Vizualizacija podatkov
 
-plot(leto,starostna_skupina)
+graf_selitve_po_starosti <- ggplot(selitve_po_starostnih_skupinah, aes(x=leto, y=starostna_skupina) + geom_line() + xlab("leto") + ylab("Starostna skupina"))
 
-graf_selitve_po_starosti <- ggplot(selitve_po_starostnih_skupinah, aes(x=leto, y=starostna_skupina))
 
-print(graf_selitve_po_starosti)
 
