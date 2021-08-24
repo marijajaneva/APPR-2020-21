b43724524276437ed94c0b446a44ea6155dbe207
Marija Janeva <mary.janeva@gmail.com>
2021-08-24 23:09:29 +0200

    oblikovanje tabele

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -9,31 +9,30 @@ library(tidyverse)
 
 #Tabela 1: Notranje selitve po regijah
 
-selitve_po_regijah <- read.csv("podatki/selitve_po regijah.csv", 
-                               skip = 2, sep =",", locale=locale(encoding = "Windows-1250"))
+selitve_po_regijah <- read.csv("podatki/selitve_po_regijah.csv", skip = 2, 
+                               header = TRUE, na.string = "-", fileEncoding = "Windows-1250", 
+                               encoding = "UT-8")
 
+colnames(selitve_po_regijah) <- c("regija_odselitve", "regija_priselitve", "leto")
 
+gather(selitve_po_regijah, "leto", "stevilo_odselitev")
 
 #Tabela 2: Notranje selitve po starostnih skupinah
 
-selitve_po_starostnih_skupinah <- read_csv2("podatki/selitve_po starostnih skupinah.csv", 
-                                           header = TRUE, skip = 2,
-                                           locale=locale(encoding = "Windows-1250"), sep = ",")
+selitve_po_starostnih_skupinah <- read.csv("podatki/selitve_po_starostnih_skupinah.csv", 
+                                           header = TRUE, skip = 2)
 
-selitve_starost <- c("leto","starostna_skupina")
+colnames(selitve_po_starostnih_skupinah) <- c("starostna_skupina", 2000:2020)
 
-leto <- c("2000","2002","2004","2005","2006","2008","2009","2011",
-          "2012","2013","2014","2015","2016","2017","2018","2019","2020")
+selitve_po_starostnih_skupinah <- selitve_po_starostnih_skupinah %>% pivot_longer(-1, names_to="leto", values_to ="stevilo_selitev")
 
-starostna_skupina <- c("0-4","5-9","10-14","15-19","20-24","25-29","30-34","35-39","40-49",
-                       "50-54","55-59","60-64","65-69","70-74","75-79","80-84","85+")
 #Tabela 3: Stanovanjske razmere
 
-stanovanjske_razmere <- read.csv("podatki/stanovanjske razmere.csv",
-                                 header = TRUE, skip = 2, sep =",")
+stanovanjske_razmere <- read.csv("podatki/stanovanjske_razmere.csv",
+                                 header = TRUE, skip = 2, sep =",", fileEncoding = "Windows-1250", encoding = "UT-8")
 
 #Tabela 4: Stanovanjski stroški
 
-stanovanjski_stroski <- read.csv("podatki/stanovanjski stroski.csv", 
-                                 header = TRUE, skip = 2, sep =",")
+stanovanjski_stroski <- read.csv("podatki/stanovanjski_stroski.csv", 
+                                 header = TRUE, skip = 2, sep =",", fileEncoding = "Windows-1250", encoding = "UT-8")
 
