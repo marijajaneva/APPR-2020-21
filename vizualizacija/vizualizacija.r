5f69676b2e8a0fbbd56c65048d89432e0ea45961
Marija Janeva <mary.janeva@gmail.com>
2021-08-24 23:31:42 +0200

    dodajanje grafa

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -1,6 +1,6 @@
 # 3. faza: Vizualizacija podatkov
 
-graf_po_starostnih_skupinah <- ggplot(selitve_po_starostnih_skupinah %>% filter(starostna_skupina == "20-24")) + aes(x=leto, y=stevilo, group=starostnaskupina)+ geom_point() + geom_line() + theme(panel.grid.major.y =element_line(color="red"))
+graf_po_starostnih_skupinah <- ggplot(selitve_po_starostnih_skupinah %>% filter(starostna_skupina == "20-22 let")) + aes(x=starostna_skupina, y=leto, group=stevilo_selitev)+ geom_point() + geom_line() + theme(panel.grid.major.y =element_line(color="red"))
 
-
+print(graf_po_starostnih_skupinah)
 
