02bf319723e78139a8983e5d633ee24433bdf3c9
Marija Janeva <mary.janeva@gmail.com>
2021-08-31 16:11:57 +0200

    popravki

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -7,9 +7,10 @@ izseljeni <- selitve_po_regijah %>% group_by(regija_odselitve, leto) %>%
 
 graf_izseljencev <- ggplot(izseljeni, aes(leto, izseljeni_sum)) +
   geom_line(aes(group = regija_odselitve, colour = factor(regija_odselitve)))+
+  theme(axis.text.x = element_text(angle = 90))+
   scale_color_discrete("Regija odselitve")+
   labs(title = "Graf notranjih selitev")+
-  xlab("Leto") +
+  xlab("Leto")
   ylab("število izseljencev")
 
 
@@ -21,7 +22,13 @@ izseljeni <- selitve_po_regijah %>% group_by(regija_odselitve, leto) %>%
 
 leto2020 <- izseljeni[izseljeni$leto == "2020" , c(1,3)]
 
-graf_iseljencev2020 <- ggplot(leto2020, aes(regija_odselitve, izseljeni_sum)) +geom_col()
+graf_iseljencev2020 <- ggplot(leto2020, aes(regija_odselitve, izseljeni_sum)) +
+  geom_col()+
+  theme(axis.text.x = element_text(angle = 90))+
+  labs(title = "Graf notranjih selitev leta 2020")+
+  xlab("Regija")+
+  ylab("Število izseljencev")
+  
 
 print(graf_iseljencev2020)
 
@@ -31,6 +38,7 @@ priseljeni <- selitve_po_regijah %>% group_by(regija_priselitve, leto) %>%
 
 graf_priseljencev <- ggplot(priseljeni, aes(leto, priseljeni_sum)) +
   geom_line(aes(group=regija_priselitve, colour = factor(regija_priselitve)))+
+  theme(axis.text.x = element_text(angle = 90))+
   scale_color_discrete("Regija priselitve")+
   labs(title = "Graf notranjih selitev")+
   xlab("Leto")+
@@ -39,6 +47,12 @@ graf_priseljencev <- ggplot(priseljeni, aes(leto, priseljeni_sum)) +
 print(graf_priseljencev)
 
 #Graf po starostnih skupinah
+
+#graf_starostna_skupina <- ggplot(data = selitve_po_starostnih_skupinah, aes(x=starostna_skupina,y=stevilo_selitev, color = leto)) + 
+#  geom_point() + facet_grid(~leto)
+
+#(graf_starostna_skupina)
+
 
 starost <- selitve_po_starostnih_skupinah %>% group_by(starostna_skupina, leto) %>%
   
@@ -49,8 +63,8 @@ leto2000 <- starost[starost$leto == "2000", c(1:3)]
 graf_starost2000 <- ggplot(leto2000, aes(starostna_skupina, starost_sum)) +
   geom_col() + labs(title = "Selitve po starostnih skupinah leta 2000")+
   xlab("Starostna skupina")+
-  ylab("Število selitev")
-
+  ylab("Število selitev") 
+  
 print(graf_starost2000)
 
 leto2007 <- starost[starost$leto == "2007", c(1:3)]
@@ -59,7 +73,8 @@ graf_starost2007 <- ggplot(leto2007, aes(starostna_skupina, starost_sum)) +
   geom_col(color = "red") + labs(title = "Selitve po starostnih skupinah leta 2007") +
   xlab("Starostna skupina")+
   ylab("Število selitev")
-           
+  
+
 print(graf_starost2007)
 
 leto2008 <- starost[starost$leto == "2008", c(1:3)]
@@ -81,7 +96,6 @@ graf_starost2020 <- ggplot(leto2020, aes(starostna_skupina, starost_sum)) +
 
 print(graf_starost2020)
 
-
 #Graf stanovanjskih razmer
 
 razmere <- stanovanjske_razmere %>% group_by(regija, kakovost_stanovanja) %>%
@@ -90,6 +104,7 @@ razmere <- stanovanjske_razmere %>% group_by(regija, kakovost_stanovanja) %>%
 
 graf_stanovanjskih_razmer <- ggplot(razmere, aes(regija, razmere_sum)) +
   geom_line(aes(group = kakovost_stanovanja, colour = factor(kakovost_stanovanja)))+
+  theme(axis.text.x = element_text(angle = 90))+
   scale_color_discrete("Kakovost stanovanja") +
   labs(title = "Kakovost stanovanja po regijah") +
   xlab("Regija")+
@@ -101,41 +116,18 @@ print(graf_stanovanjskih_razmer)
 
 #Graf stanovanjskih stroškov
 
-stroski <- stanovanjski_stroski %>% group_by(regija,breme_stroskov)%>%
-  
-  summarize(stroski_sum = sum(stevilo_gospodinjstev))
-
-nizko_breme <- stroski[stroski$breme_stroskov == "Nizko" , c(1:3)]
-
-graf_nizko_breme <-ggplot(nizko_breme, aes(regija, stroski_sum))+
-  geom_col() + labs(title = "Število gospodinjstev,katerim stroški stanovanja predstavljajo nizko breme")+
+graf_stroski <- ggplot(stanovanjski_stroski, aes(x=leto,y=stevilo_gospodinjstev, fill=breme_stroskov))+
+  geom_bar(stat="identity", position = "dodge") + 
+  scale_color_discrete("Breme stroškov")+
+  labs(title = "Kakšno breme prebivalcem predstavljajo stanovanjski stroški po letih")+
   xlab("Leto")+
   ylab("Število gospodinjstev")
 
-print(graf_nizko_breme)
-
-srednje_breme <- stroski[stroski$breme_stroskov == "Srednje", c(1:3)]
-
-graf_srednje_breme <- ggplot(srednje_breme, aes(regija, stroski_sum))+
-  geom_col()+
-  labs(title = "Število gospodinjstev, katerim stroški stanovanja predstavljajo srednje breme")+
-  xlab("Leto")+
-  ylab("Število gospodinjstev")
-
-print(graf_srednje_breme) 
-
-veliko_breme <- stroski[stroski$breme_stroskov == "Veliko", c(1:3)]
-
-graf_veliko_breme <- ggplot(veliko_breme, aes(regija, stroski_sum))+
-  geom_col() + 
-  labs(title = "Število gospodinjstev, katerim stroški stanovanja predstavljajo veliko breme")+
-  xlab("Leto")+
-  ylab("Število gospodinjstev")
-
-print(graf_veliko_breme)
+print(graf_stroski)
 
 #Zemljevid
 
 zemljevid <- uvozi.zemljevid("http://biogeo.ucdavis.edu/data/gadm2.8/shp/SVN_adm_shp.zip",
                              "SVN_adm1", encoding = "UTF-8")
 
+print(zemljevid)
