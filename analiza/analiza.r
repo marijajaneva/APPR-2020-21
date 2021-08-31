7e73603eaba029e9c85c4f85e6281bba5953e43f
Marija Janeva <mary.janeva@gmail.com>
2021-08-31 17:04:39 +0200

    linearna regresija

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -1,6 +1,14 @@
 # 4. faza: Analiza podatkov
 
-zemljevid <- uvozi.zemljevid("http://biogeo.ucdavis.edu/data/gadm2.8/shp/SVN_adm_shp.zip",
-                             "SVN_adm1", encoding = "UTF-8") %>% pretvori.zemljevid()%>%
+#LINEARNA REGRESIJA
 
+#Linearen model med stanovanjskimi razmerami in regije izselitve
+fit <- lm(stevilo_stanovanj ~ kakovost_stanovanja , data=stanovanjske_razmere)
+summary(fit)
+graf <- ggplot(stanovanjske_razmere, aes(x = kakovost_stanovanja  , y = stanovanjske_razmere$stevilo_stanovanj)) + 
+  geom_point() + 
+  geom_smooth(method=lm, se=FALSE) + labs(x = "Kakovost stanovanja", y = "Število stanovanj v slabem stanju", fill = NULL)
+
+
+print(graf)
 
